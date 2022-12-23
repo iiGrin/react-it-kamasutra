cd6c6c318b5ea12cd6922419a733c3d2b2365f19
iiGrin <devdevgrin@gmail.com>
2022-12-23 17:38:19 +0300

    create firsts components

--- 01-first-project/react-kabzda-1/src/App.js
+++ 01-first-project/react-kabzda-1/src/App.js
@@ -2,9 +2,23 @@ import logo from './logo.svg';
 import './App.css';
 
 
+// inserted components
 const App = () => {
   return (
-    <div className="App">
+    <div>
+      <Header />
+      <div>
+        <Technologies />
+      </div>
+    </div>
+  );
+}
+
+
+// create components
+const Technologies = () => {
+  return (
+    <div>
       <ul>
         <li>CSS</li>
         <li>HTML</li>
@@ -12,7 +26,16 @@ const App = () => {
         <li>REACT</li>
       </ul>
     </div>
-  );
+  )
+}
+
+const Header = () => { 
+  return (<div>
+          <a href="#">Home</a>
+          <a href="#">News</a>
+          <a href="#">Feed</a>
+          <a href="#">Messages</a>
+          </div>);
 }
 
 export default App;
