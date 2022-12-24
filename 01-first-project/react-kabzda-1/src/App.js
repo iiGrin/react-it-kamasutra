402b5c0092b06f4df7609b30e2f1a188df202340
iiGrin <devdevgrin@gmail.com>
2022-12-24 15:40:05 +0300

    making the basic structure of the main page

--- 01-first-project/react-kabzda-1/src/App.js
+++ 01-first-project/react-kabzda-1/src/App.js
@@ -6,9 +6,51 @@ import './App.css';
 // inserted components
 const App = () => {
   return (
-    <div>
-      <Header />
-      <Technologies />
+    <div className="app-wrapper">
+      <header className='header'>
+        <img src='https://i.pinimg.com/originals/e9/e2/78/e9e2787d0cb55d570fe1c76843506759.jpg'></img>
+        <nav className='nav'>
+          <div>
+            <a href='#'>Profile</a>
+          </div>
+          <div>
+            <a href='#'>Messages</a>
+          </div>
+          <div>
+            <a href='#'>News</a>
+          </div>
+          <div>
+            <a href='#'>Music</a>
+          </div>
+          <div>
+            <a href='#'>Settings</a>
+          </div>
+          <div></div>
+          <div></div>
+        </nav>
+        <div className='content'>
+          <div>
+            <img src='https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg' />
+          </div>
+          <div>
+            ava + disc
+          </div>
+          <div>
+            my post
+            <div>
+              new post
+            </div>
+            <div>
+              <div>
+                post1
+              </div>
+              <div>
+                post2
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
     </div>
   );
 }
