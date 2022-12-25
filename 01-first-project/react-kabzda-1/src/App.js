9ba009cbda9b20f309c83d07abd72c97d8d9a0bf
iiGrin <devdevgrin@gmail.com>
2022-12-25 14:27:35 +0300

    move components to different files

--- 01-first-project/react-kabzda-1/src/App.js
+++ 01-first-project/react-kabzda-1/src/App.js
@@ -1,56 +1,18 @@
 import React from 'react';
 //import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import Navbar from './components/Navbar';
+import Profile from './components/Profile';
 
 
 // inserted components
 const App = () => {
   return (
     <div className="app-wrapper">
-      <header className='header'>
-        <img src='https://i.pinimg.com/originals/e9/e2/78/e9e2787d0cb55d570fe1c76843506759.jpg'></img>
-        <nav className='nav'>
-          <div>
-            <a href='#'>Profile</a>
-          </div>
-          <div>
-            <a href='#'>Messages</a>
-          </div>
-          <div>
-            <a href='#'>News</a>
-          </div>
-          <div>
-            <a href='#'>Music</a>
-          </div>
-          <div>
-            <a href='#'>Settings</a>
-          </div>
-          <div></div>
-          <div></div>
-        </nav>
-        <div className='content'>
-          <div>
-            <img src='https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg' />
-          </div>
-          <div>
-            ava + disc
-          </div>
-          <div>
-            my post
-            <div>
-              new post
-            </div>
-            <div>
-              <div>
-                post1
-              </div>
-              <div>
-                post2
-              </div>
-            </div>
-          </div>
-        </div>
-      </header>
+      <Header />
+      <Navbar />
+      <Profile />
     </div>
   );
 }
---
+++ 01-first-project/react-kabzda-1/src/components/Profile.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Profile = () => {
+    return <div className='profile'>
+        <div>
+            <img src='https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg' />
+        </div>
+        <div>
+            ava + disc
+        </div>
+        <div>
+            my post
+            <div>
+                new post
+            </div>
+            <div>
+                <div>
+                    post1
+                </div>
+                <div>
+                    post2
+                </div>
+            </div>
+        </div>
+    </div>
+}
+
+export default Profile
