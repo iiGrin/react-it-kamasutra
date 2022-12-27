ab6b30737e8e31a2ea109168b61653f5223567c7
iiGrin <devdevgrin@gmail.com>
2022-12-27 18:42:00 +0300

    create Dialogs page

--- 01-first-project/react-kabzda-1/src/App.js
+++ 01-first-project/react-kabzda-1/src/App.js
@@ -4,15 +4,20 @@ import './App.css';
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
+import Dialogs from './components/Dialogs/Dialogs';
+import classes from './App.css'
 
 
 // inserted components
 const App = () => {
   return (
-    <div className="app-wrapper">
+    <div className='app-wrapper'>
       <Header />
       <Navbar />
-      <Profile />
+      <div class='app-wrapper-content'>
+        <Dialogs />
+        {/* <Profile /> */}
+      </div>
     </div>
   );
 }
--- 01-first-project/react-kabzda-1/src/components/Profile/Profile.jsx
+++ 01-first-project/react-kabzda-1/src/components/Profile/Profile.jsx
@@ -4,7 +4,7 @@ import './Profile.module.css';
 import classes from './Profile.module.css'; // import css module .module.css
 
 const Profile = () => {
-    return <div className={classes.content}>
+    return <div>
         <div>
             <img src='https://cdn.pixabay.com/photo/2018/01/14/23/12/nature-3082832_960_720.jpg' />
         </div>
