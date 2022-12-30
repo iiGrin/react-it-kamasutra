1c7bd373661a446d29f7bc77fdb5c745d66ae7ec
iiGrin <devdevgrin@gmail.com>
2022-12-30 13:38:58 +0300

    myPosts refactoring: postsDataArray

--- 01-first-project/react-kabzda-1/src/components/Profile/MyPosts/MyPosts.jsx
+++ 01-first-project/react-kabzda-1/src/components/Profile/MyPosts/MyPosts.jsx
@@ -4,11 +4,15 @@ import classes from './MyPosts.module.css'; // import css module .module.css
 import Post from './Post/Post';
 
 const MyPosts = () => {
+
+    let postsData = [
+        { id: 1, message: 'Hi. How are you?', likesCount: 12 },
+        { id: 2, message: 'Its my first post', likesCount: 11 },
+    ]
+
     return (
         <div className={classes.postsBlock}>
-            <h3>
-                My post
-            </h3>
+            <h3>My post</h3>
             <div>
                 <div>
                     <textarea></textarea>
@@ -18,8 +22,8 @@ const MyPosts = () => {
                 </div>
             </div>
             <div className={classes.posts}>
-                <Post message='Hi. How are you?' likesCount='1' />
-                <Post message='Its my first post' likesCount='23' />
+                <Post message={postsData[0].message} likesCount={postsData[0].likesCount} />
+                <Post message={postsData[1].message} likesCount={postsData[1].likesCount} />
             </div>
         </div>
     )
