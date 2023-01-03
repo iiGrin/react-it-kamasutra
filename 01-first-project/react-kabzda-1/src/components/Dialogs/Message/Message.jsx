264b550edf556431d81934b894c576e75078d591
iiGrin <devdevgrin@gmail.com>
2023-01-03 16:32:11 +0300

    Dialogs refactoring

---
+++ 01-first-project/react-kabzda-1/src/components/Dialogs/Message/Message.jsx
@@ -0,0 +1,8 @@
+import { NavLink } from 'react-router-dom'
+import classes from './../Dialogs.module.css'
+
+const Message = (props) => {
+    return <div className={classes.message}>{props.message}</div>
+}
+
+export default Message
