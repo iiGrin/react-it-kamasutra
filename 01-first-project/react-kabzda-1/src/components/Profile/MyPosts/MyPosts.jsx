fe01f68751461975c796edb51b363e155c87be3f
iiGrin <devdevgrin@gmail.com>
2023-01-07 14:59:20 +0300

    onClick: addPost

--- 01-first-project/react-kabzda-1/src/components/Profile/MyPosts/MyPosts.jsx
+++ 01-first-project/react-kabzda-1/src/components/Profile/MyPosts/MyPosts.jsx
@@ -8,15 +8,22 @@ const MyPosts = (props) => {
     // auto adding post from posts array 
     let postsElements = props.posts.map(post => <Post message={post.message} likesCount={post.likesCount} />)
 
+    let newPostElement = React.createRef()
+
+    let addPost = () => {
+        let text = newPostElement.current.value
+        alert(text)
+    }
+
     return (
         <div className={classes.postsBlock}>
             <h3>My post</h3>
             <div>
                 <div>
-                    <textarea></textarea>
+                    <textarea ref={newPostElement}></textarea>
                 </div>
                 <div>
-                    <button>Add post</button>
+                    <button onClick={addPost}>Add post</button>
                 </div>
             </div>
             <div className={classes.posts}>
