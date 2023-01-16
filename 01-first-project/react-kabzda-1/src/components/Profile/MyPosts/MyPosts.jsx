080c1069da24d09375f42825fb34b93230be8433
iiGrin <devdevgrin@gmail.com>
2023-01-16 15:29:02 +0300

    actionCreator, actionType

--- 01-first-project/react-kabzda-1/src/components/Profile/MyPosts/MyPosts.jsx
+++ 01-first-project/react-kabzda-1/src/components/Profile/MyPosts/MyPosts.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './MyPosts.module.css';
 import classes from './MyPosts.module.css'; // import css module .module.css
 import Post from './Post/Post';
+import { addPostActionCreator, updateNewPostTextActionCreator } from '../../../Redux/State';
 
 const MyPosts = (props) => {
     // auto adding post from posts array 
@@ -10,12 +11,12 @@ const MyPosts = (props) => {
     let newPostElement = React.createRef()
 
     let addPost = () => {
-        props.dispatch({ type: 'ADD-POST' })
+        props.dispatch(addPostActionCreator())
     }
 
     let onPostChange = () => {
         let text = newPostElement.current.value
-        let action = { type: 'UPDATE-NEW-POST-TEXT', newText: text }
+        let action = updateNewPostTextActionCreator(text)
         props.dispatch(action)
     }
 
