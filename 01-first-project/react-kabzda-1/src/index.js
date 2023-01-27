f187565f9e66d5b3a2fb25062ecf06a100f3a397
iiGrin <devdevgrin@gmail.com>
2023-01-27 14:30:42 +0300

    ProfileReducer copy

--- 01-first-project/react-kabzda-1/src/index.js
+++ 01-first-project/react-kabzda-1/src/index.js
@@ -3,25 +3,19 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import store from './Redux/ReduxStore'
 import { Provider } from 'react-redux';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-let rerenderEntireTree = (state) => {
-  root.render(
-    <React.StrictMode>
-      <Provider store={store}>
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
       <App />
-      </Provider>
-    </React.StrictMode>
-  );
+    </Provider>
+  </React.StrictMode>
+);
 
-}
 
-rerenderEntireTree(store.getState())
 
-store.subscribe( () => {
-  let state = store.getState()
-  rerenderEntireTree(state)
-})
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
