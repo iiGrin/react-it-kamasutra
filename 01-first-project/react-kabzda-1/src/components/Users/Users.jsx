ae5c3b3eda27894592b3f3ed70a70f11f9b08308
iiGrin <devdevgrin@gmail.com>
2023-02-04 16:16:04 +0300

    side effect

--- 01-first-project/react-kabzda-1/src/components/Users/Users.jsx
+++ 01-first-project/react-kabzda-1/src/components/Users/Users.jsx
@@ -5,16 +5,18 @@ import userPhoto from '../../Assets/Images/userPhoto.png'
 
 let Users = (props) => {
 
-    if (props.users.length === 0) {
-
-        axios.get('https://social-network.samuraijs.com/api/1.0/users').then( response => {
-            props.setUsers(response.data.items)
-        })
-
-        
+    let getUsers = () => {
+        if (props.users.length === 0) {
+            axios.get('https://social-network.samuraijs.com/api/1.0/users')
+                .then(response => {
+                    props.setUsers(response.data.items)
+                })
+        }
     }
 
+
     return <div>
+        <button onClick={getUsers}>Get Users</button>
         {
             props.users.map(u => <div key={u.id}>
                 <span>
