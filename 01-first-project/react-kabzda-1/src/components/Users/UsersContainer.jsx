078f12953a087d439b4d7654f2e9b5269918a419
iiGrin <devdevgrin@gmail.com>
2023-02-10 17:25:50 +0300

    isFetching: Preloader

--- 01-first-project/react-kabzda-1/src/components/Users/UsersContainer.jsx
+++ 01-first-project/react-kabzda-1/src/components/Users/UsersContainer.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import { connect } from "react-redux";
-import { followAC, setCurrentPageAC, setUsersAC, setTotalUsersCountAC, unfollowAC } from "../../Redux/UsersReducer";
+import { followAC, setCurrentPageAC, setUsersAC, setTotalUsersCountAC, unfollowAC, toggleIsFetchingAC } from "../../Redux/UsersReducer";
 import axios from "axios";
-import Users from "./Users"
+import Users from "./Users";
+import preloader from "./../../Assets/Images/preloader.svg";
+import Preloader from "../common/Preloader/Preloader";
 
 class UsersContainer extends React.Component {
 
     componentDidMount() {
+        this.props.toggleIsFetching(true)
         axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
             .then(response => {
+                this.props.toggleIsFetching(false)
                 this.props.setUsers(response.data.items)
                 this.props.setTotalUsersCount(response.data.totalCount)
             })
@@ -16,22 +20,28 @@ class UsersContainer extends React.Component {
 
     onPageChanged = (pageNumber) => {
         this.props.setCurrentPage(pageNumber)
+        this.props.toggleIsFetching(true)
         axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`)
             .then(response => {
+                this.props.toggleIsFetching(false)
                 this.props.setUsers(response.data.items)
             })
     }
 
     render() {
-        return <Users
-            totalUsersCount={this.props.totalUsersCount}
-            pageSize={this.props.pageSize}
-            currentPage={this.props.currentPage}
-            onPageChanged={this.onPageChanged}
-            users={this.props.users}
-            follow={this.props.follow}
-            unfollow={this.props.unfollow}
-        />
+        return <>
+            {this.props.isFetching ?
+                <Preloader /> : null}
+            <Users
+                totalUsersCount={this.props.totalUsersCount}
+                pageSize={this.props.pageSize}
+                currentPage={this.props.currentPage}
+                onPageChanged={this.onPageChanged}
+                users={this.props.users}
+                follow={this.props.follow}
+                unfollow={this.props.unfollow}
+            />
+        </>
     }
 }
 
@@ -40,7 +50,8 @@ let mapStateToProps = (state) => {
         users: state.usersPage.users,
         pageSize: state.usersPage.pageSize,
         totalUsersCount: state.usersPage.totalUsersCount,
-        currentPage: state.usersPage.currentPage
+        currentPage: state.usersPage.currentPage,
+        isFetching: state.usersPage.isFetching
     }
 }
 
@@ -60,7 +71,10 @@ let mapDispatchToProps = (dispatch) => {
         },
         setTotalUsersCount: (totalCount) => {
             dispatch(setTotalUsersCountAC(totalCount))
-        }
+        },
+        toggleIsFetching: (isFetching) => {
+            dispatch(toggleIsFetchingAC(isFetching))
+        },
     }
 }
 
---
+++ 01-first-project/react-kabzda-1/src/components/common/Preloader/Preloader.js
@@ -0,0 +1,9 @@
+import preloader from './../../../Assets/Images/preloader.svg'
+
+let Preloader = (props) => {
+    return <div>
+    <img src={preloader} />
+</div>
+}
+
+export default Preloader
